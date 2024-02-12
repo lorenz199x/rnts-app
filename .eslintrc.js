124637f4070a84b80e60ccaacf5c74a93466520d
lo <lorenz.orbita@veritaspay.com>
2024-02-12 22:42:27 +0800

    - install typescript - setup Prettier, eslint, husky, babel config

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,4 +1,40 @@
 module.exports = {
   root: true,
   extends: '@react-native',
+  plugins: ['unused-imports', 'simple-import-sort'],
+  rules: {
+    'no-unused-vars': 'off',
+    'eslint-comments/no-unlimited-disable': 'off',
+    'unused-imports/no-unused-imports': 'error',
+    'unused-imports/no-unused-vars': [
+      'warn',
+      {
+        vars: 'all',
+        varsIgnorePattern: '^_',
+        args: 'after-used',
+        argsIgnorePattern: '^_',
+      },
+    ],
+    'simple-import-sort/exports': 'error',
+    'simple-import-sort/imports': [
+      'error',
+      {
+        groups: [
+          ['^\\u0000'],
+          ['^react', '^@?\\w'],
+          ['^(@|components)(/.*|$)'],
+          ['^\\.\\.(?!/?$)', '^\\.\\./?$'],
+          ['^\\./(?=.*/)(?!/?$)', '^\\.(?!/?$)', '^\\./?$'],
+          ['^.+\\.?(css)$'],
+        ],
+      },
+    ],
+    'react-native/no-inline-styles': 'error',
+    'prettier/prettier': [
+      'error',
+      {
+        endOfLine: 'auto',
+      },
+    ],
+  },
 };
--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,26 @@
 module.exports = {
-  presets: ['module:@react-native/babel-preset'],
+  presets: ['module:metro-react-native-babel-preset'],
+  plugins: [
+    'react-native-reanimated/plugin',
+    [
+      'module-resolver',
+      {
+        root: ['.'],
+        alias: {
+          '@mocks': './__mocks__',
+          '@tests': './__tests__',
+          '@temps': './__temps__',
+          '@assets': './src/assets',
+          '@components': './src/components',
+          '@navigation': './src/navigation',
+          '@redux': './src/redux',
+          '@screens': './src/screens',
+          '@shared': './src/shared',
+          '@utils': './src/utils',
+          '@styles': './src/styles',
+          '@themes': './src/themes',
+        },
+      },
+    ],
+  ],
 };
