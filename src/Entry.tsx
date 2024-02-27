a5c3047abaf5b3d78aa2e1aa996c940016c4e291
lorenz199x <lorenz.orbita@veritaspay.com>
2024-02-27 12:30:46 +0800

    - setup multiple environment for android and ios

--- src/Entry.tsx
+++ src/Entry.tsx
@@ -1,10 +1,17 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import Config from 'react-native-config';
+import logger from '@utils/logger';
 
 const Entry = () => {
+  useEffect(() => {
+    console.log(Config.NODE_ENV);
+    logger(Config.NODE_ENV);
+  });
+
   return (
     <View style={styles.container}>
-      <Text>Entry</Text>
+      <Text>Entry: {Config.NODE_ENV}</Text>
     </View>
   );
 };
