4edc85fc095e7859057c4a23754bddbbc231a1e6
lorenz199x <lorenzorbita@gmail.com>
2024-04-03 10:52:01 +0800

    - change package name - bible verse app

--- android/app/src/main/java/com/rnts/MainApplication.kt
+++ android/app/src/main/java/com/rnts/MainApplication.kt
@@ -1,4 +1,4 @@
-package com.rnts
+package com.lo.yaverse
 
 import android.app.Application
 import com.facebook.react.PackageList
--- ios/rntsTests/rntsTests.m
+++ ios/rntsTests/rntsTests.m
@@ -7,11 +7,11 @@
 #define TIMEOUT_SECONDS 600
 #define TEXT_TO_LOOK_FOR @"Welcome to React"
 
-@interface rntsTests : XCTestCase
+@interface YA Bible PromiseTests : XCTestCase
 
 @end
 
-@implementation rntsTests
+@implementation YA Bible PromiseTests
 
 - (BOOL)findSubviewInView:(UIView *)view matching:(BOOL (^)(UIView *view))test
 {
